6710acf79c260c771675a3db180c5827aa905e34
Hawkar Saeed <hawkarsaeed@yahoo.co.uk>
2025-11-24 23:50:12 +0300

    Remove n

--- src/middleware/authMiddleware.ts
+++ src/middleware/authMiddleware.ts
@@ -2,19 +2,26 @@
 import { Request, Response, NextFunction } from "express";
 import jwt from "jsonwebtoken";
 
-export const authenticateAdmin = (req: Request, res: Response, next: NextFunction) => {
+export const authenticateAdmin = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   const token = req.headers.authorization?.split(" ")[1]; // Get the token from the header
 
   if (!token) {
-    return res.status(401).json({ message: "Access denied. No token provided." });
+    return res
+      .status(401)
+      .json({ message: "Access denied. No token provided." });
   }
 
   try {
     // Verify the token
-    const decoded = jwt.verify(token, "your-secret-key");
+    const JWT_SECRET = process.env.JWT_SECRET || "fallback-secret";
+    const decoded = jwt.verify(token, JWT_SECRET);
     (req as any).user = decoded; // Attach the decoded user to the request object
     next();
-} catch (error) {
-  res.status(400).json({ message: "Invalid token" });
-}
+  } catch (error) {
+    res.status(400).json({ message: "Invalid token" });
+  }
 };
--- src/models/Admin.ts
+++ src/models/Admin.ts
@@ -7,9 +7,14 @@ interface Admin {
 }
 
 // Example admin (for demonstration purposes)
-const admin: Admin = {
-  username: "admin",
-  password: bcrypt.hashSync("admin123", 10), // Hash the password
-};
+// const admin: Admin = {
+//   username: "admin",
+//   password: bcrypt.hashSync("admin123", 10), // Hash the password
+// };
 
+// To environment variables:
+const admin: Admin = {
+  username: process.env.ADMIN_USERNAME || "admin",
+  password: bcrypt.hashSync(process.env.ADMIN_PASSWORD || "admin123", 10),
+};
 export default admin;
